a70bef026241e4b8d99db385fdb55810594653a1
Rahul-Gautam-7 <rahulgautam7r@gmail.com>
2024-05-28 18:51:29 +0530

    margin

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
   
     <BrowserRouter>
     <a href='/login' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"> login </a>
-    <a href='/app'className='text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'> App </a>
+    <a href='/app'className='mx-4 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'> App </a>
     <Routes>
 
       <Route  path='/login' element={<Login />}  />
