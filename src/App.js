30ecb8576140658399c5c29a85a111b615b3b1cf
Rahul Gautam <rahulgautam7r@gmail.com>
2024-05-29 10:04:14 +0530

    login form completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Head from './Head';
 import Bod from './Bod';
 import Card from './Card';
+import Lg from './Lg';
 import './App.css';
 
 function App() {
   return (
   <>
    <div className='container mx-auto'>
+    
   <Head />
   <Bod />
+  
 
   </div>
   </>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Lg from './Lg';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Login from './Login';
+
 
 
 
@@ -11,20 +12,20 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-   
+
   
-    <BrowserRouter>
+    <BrowserRouter >
     <a href='/login' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"> login </a>
     <a href='/app'className='mx-4 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'> App </a>
     <Routes>
 
-      <Route  path='/login' element={<Login />}  />
+    <Route  path='/login' element={<Lg />}  />
       <Route  path='/app' element={<App />}  />
 
     </Routes>
     
     </BrowserRouter>
-   
+ 
     
   
     
