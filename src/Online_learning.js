7561bbf18dffbb7e83647ed3b90c799bc6ff8185
Rahul-Gautam-7 <rahulgautam7r@gmail.com>
2024-05-29 20:12:19 +0530

    institute

--- src/Online_learning.js
+++ src/Online_learning.js
@@ -17,7 +17,7 @@ function Online_learning() {
     <>
     <div className='container'>
       <div className='row'>
-        <div className='grid grid-cols-3 my-16 ml-24 '>
+        <div className='grid grid-cols-3 my-16  '>
          {data.map(function(s)
             {
               return <Institute />
